af0ec1097def2b558522c9a7c0c2755c3ae472b9
glitch927 <chichijun09@gmail.com>
2026-05-27 16:32:18 +0900

    2025-05-27/04:32

---
+++ js_study/myself/menu flipper/js/script.js
@@ -0,0 +1,13 @@
+const menuflip = ["치킨", "피자", "햄버거", "순대국밥", "삼겹살", "목살", "까르보나라", "김치볶음밥", "간장계란밥"];
+const btn = document.getElementById("btn");
+const menu = document.querySelector(".menu");
+
+btn.addEventListener("click", function() {
+    result = getRandomMenu();
+
+    menu.textContent = menuflip[result];
+});
+
+function getRandomMenu() {
+    return Math.floor(Math.random()*menuflip.length);
+}
