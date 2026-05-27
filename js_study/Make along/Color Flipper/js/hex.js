ed4c9eb53bdb3097d5561c316bcb272a29a5082e
glitch927 <chichijun09@gmail.com>
2026-05-27 16:47:44 +0900

    color copy

--- js_study/Make along/Color Flipper/js/hex.js
+++ js_study/Make along/Color Flipper/js/hex.js
@@ -10,6 +10,17 @@ btn.addEventListener('click', function() {
 
     document.body.style.backgroundColor = hexColor;
     color.textContent = hexColor;
+
+    // 여기서 복사 기능을 실행합니다!
+    navigator.clipboard.writeText(hexColor)
+      .then(() => {
+          // 복사가 성공하면 알림창을 띄웁니다.
+          alert(`복사 완료: ${hexColor}`);
+      })
+      .catch((err) => {
+          // (선택사항) 만약 보안 문제나 오류 때문에 복사가 실패하면 이쪽이 실행됩니다.
+          console.error("복사 실패 ㅠㅠ", err);
+      });
 });
 
 function getRandomNumber() {
