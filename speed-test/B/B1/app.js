840e1fe88c56bd1a7b90b1a360d11665e92190ef
glich <chichijun09@gmail.com>
2026-09-16 14:02:06 +0900

    Add files via upload

---
+++ speed-test/B/B1/app.js
@@ -0,0 +1,20 @@
+const password = document.querySelector('input');
+const text = document.querySelector('p');
+
+password.addEventListener('input', () => {
+    if (/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-za-z0-9\s]).{8}/.test(password.value)) {
+        password.style.border = '2px solid green';
+        text.textContent = '강함';
+        text.style.color = 'green';
+    }
+    else if (/^(?=.*[A-Z])(?=.*\d).{6}/.test(password.value)) {
+        password.style.border = '2px solid orange';
+        text.textContent = '보통';
+        text.style.color = 'orange';
+    }
+    else if (password.value.length < 6 ) {
+        password.style.border = '2px solid red';
+        text.textContent = '약함';
+        text.style.color = 'red';
+    }
+})
